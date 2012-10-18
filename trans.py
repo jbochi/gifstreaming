e384b4e994a0b07fd6aa65ed6c00d6330857ab5c
Juarez Bochi <jbochi@gmail.com>
2012-10-18 15:56:25 -0300

    Change global color table to local color table

--- trans.py
+++ trans.py
@@ -1,47 +1,51 @@
 # -*- coding: utf-8 -*-
 
-# CABEÇALHO DE ARQUIVO COMUM
-# 00000000  47 49 46 38 39 61 9f 00  61 00 f7 1f 00 00 00 00  |GIF89a..a.......|
-# ...
-# 00000300  ff 90 fc ff b4 fc ff d8  fc ff fc fc ff 2c # linha onde começa Image Descriptor
-# ...
-# 000006c0  0e 12 10 00 3b # 3b -> File terminator
-
-
-# CABEÇALHO DE FRAME
-# 21 f9                   # Graphic Control Extension for frame #1
-# 04                      # four bytes of data follow
-# 04					  # no transparency ??
-# 0d 00                   # 0.10s sec delay before painting next frame
-# 1f				      # no transparent color ???
-# 00					  # end of GCE block
-# 2c
+# GIF Spec: http://tronche.com/computer-graphics/gif/gif89a.html#image-descriptor
+# Better explanation: http://www.matthewflickinger.com/lab/whatsinagif/bits_and_bytes.asp
 
 def full_gif_to_frame(raw_gif):
-	assert raw_gif.startswith("GIF89a")
-	width = ord(raw_gif[6]) + 256 * ord(raw_gif[7])
-	height = ord(raw_gif[8]) + 256 * ord(raw_gif[9])
+    # Header
+    assert raw_gif.startswith("GIF89a")
+    width = ord(raw_gif[6]) + 256 * ord(raw_gif[7])
+    height = ord(raw_gif[8]) + 256 * ord(raw_gif[9])
 
-	assert hex(ord(raw_gif[10])) == "0xf7" # gct code
-	gct_len = 256
+    # Global color table
+    assert hex(ord(raw_gif[10])) == "0xf7"
+    gct_len = 256
+    gct_range = 13, 13 + (gct_len * 3)
+    color_table = raw_gif[gct_range[0]:gct_range[1]]
 
-	gct_range = 13, 13 + (gct_len * 3)
-	gct = raw_gif[gct_range[0]:gct_range[1]]
+    # Image descriptor
+    image_descriptor_range = gct_range[1], gct_range[1] + 10
+    image_descriptor = raw_gif[image_descriptor_range[0] : image_descriptor_range[1]]
+    assert image_descriptor[0] == ","
+    assert ord(image_descriptor[-1]) == 0        # Packed Fields
+    packed_fields = chr(int("10000111", base=2)) # Local Color Table Flag        1 Bit
+                                                 # Interlace Flag                1 Bit
+                                                 # Sort Flag                     1 Bit
+                                                 # Reserved                      2 Bits
+                                                 # Size of Local Color Table     3 Bits
 
-	assert raw_gif[gct_range[1]] == "," # Image descriptor
+    new_image_descriptor = image_descriptor[:-1] + packed_fields
 
-	data_range = gct_range[1], len(raw_gif) - 1
-	assert raw_gif[data_range[1]] == ";" # file terminator
-	data = raw_gif[data_range[0] : data_range[1]]
+    # Image data
+    data_range = image_descriptor_range[1], len(raw_gif) - 1
+    data = raw_gif[data_range[0] : data_range[1]]
+    assert raw_gif[data_range[1]] == ";" # file terminator
 
-	frame = ''.join([b.decode('hex') for b in "21 f9 04 04 0d 00 1f 00".split(" ")])
-	frame += data
-	return frame
+    # New frame data
+    frame = ''.join([b.decode('hex') for b in "21 f9 04 04 0d 00 1f 00".split(" ")])
+    frame += new_image_descriptor
+    frame += color_table
+    frame += data
+
+    return frame
+
 
 if __name__ == "__main__":
-	for i in xrange(1, 100):
-		print i
-		with open("full/frame%d.gif" % i, "rb") as f:
-			raw_gif = f.read()
-		with open("split/out%d.part" % i, "wb") as f:
-			f.write(full_gif_to_frame(raw_gif))
+  for i in xrange(1, 500):
+      print i
+      with open("full/frame%d.gif" % i, "rb") as f:
+          raw_gif = f.read()
+      with open("split/out%d.part" % i, "wb") as f:
+          f.write(full_gif_to_frame(raw_gif))
